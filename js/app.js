0ef66debf802fb760f2661bb71cf7f0c2bd2ca84
Connor <connorbfranklin@gmail.com>
2014-12-16 16:35:12 -0500

    hour 3-4.5: details with fancy box, isotope filters (need work on overlaps), some responsive work

--- js/app.js
+++ js/app.js
@@ -1,10 +1,19 @@
 var $sutra;
+var $filters;
+var $sutraPos;
+var $container;
 
 $(function(){
 	$sutra = $('.cancer-sutra');
+	$sutraPos = $('.cancer-sutra > div');
+	$filters = $('.filter a');
+	$container = $('.container');
 	$sutra.isotope();
 	imagesLoaded('body',begin)
 });
+
+
+
 
 function begin(){
 	if(document.body.scrollTop === 0){
@@ -16,6 +25,7 @@ function begin(){
 	}
 	function intro(){
 		$('body').removeClass('intro');
+		$sutra.isotope('layout');
 		if(scrolledTop){
 			setTimeout(showSutra,1500)
 		}
@@ -23,8 +33,51 @@ function begin(){
 			showSutra();
 		}
 		function showSutra(){
-			$sutra.show();
-			new WOW().init();
+			$sutra.removeClass('opacity0');
+			//new WOW().init();
 		}
+		bindings();
 	}
+}
+
+function bindings(){
+	//filters
+	$filters.on('click',function(e){
+		$sutra.find('div').show();
+		if($(this).data('toggle') === 'off'){
+			$(this).data('toggle','on').attr('data-toggle','on')
+		}
+		else{
+			$(this).data('toggle','off').attr('data-toggle','off')
+		}
+		var filterArray = []
+		$filters.each(function(){
+			console.log($(this).parents('.filter').data('filter-type'))
+			if($(this).data('toggle') === 'on'){
+				var filterType = $(this).parents('.filter').data('filter-type');
+				var filterValue = $(this).data('filter')
+				filterArray.push([filterType,filterValue]);
+			}
+		});
+		for(i in filterArray){
+			filterArray[i] = '[data-'+filterArray[i][0]+'="'+filterArray[i][1]+'"]';
+		}
+		console.log(filterArray)
+		var theFilter = filterArray.join(',');
+		console.log(theFilter)
+		$sutra.isotope({ filter: theFilter });
+		e.preventDefault();
+	});
+	//detail
+	$sutraPos.on('click',function(){
+		var fancyWidth = $container.width();
+		var $fancyContent = $(this).find('.detail');
+		console.log(fancyWidth)
+		$.fancybox.open({
+			autoSize: false,
+			width: '100%',
+			content: $fancyContent,
+			scrolling: 'no'
+		})
+	})
 }
