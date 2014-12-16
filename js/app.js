78c98c6295fc9ecac0cea6c5974b6f0f13501058
Connor <connorbfranklin@gmail.com>
2014-12-16 01:38:06 -0500

    add load animations, add fancy box, add formatting, begin responsive

--- js/app.js
+++ js/app.js
@@ -1,8 +1,30 @@
+var $sutra;
+
 $(function(){
-	$('.cancer-sutra').isotope({
-		//layoutMode: 'packery'
-		//layoutMode: 'spineAlign'
-		//layoutMode: 'centered'
-	});
-	new WOW().init();
+	$sutra = $('.cancer-sutra');
+	$sutra.isotope();
+	imagesLoaded('body',begin)
 });
+
+function begin(){
+	if(document.body.scrollTop === 0){
+		var scrolledTop = true;
+		setTimeout(intro,1500);
+	}
+	else{
+		intro(); 
+	}
+	function intro(){
+		$('body').removeClass('intro');
+		if(scrolledTop){
+			setTimeout(showSutra,1500)
+		}
+		else{
+			showSutra();
+		}
+		function showSutra(){
+			$sutra.show();
+			new WOW().init();
+		}
+	}
+}
