02c6e5b63be9b9d25f13866383f2d7320c8f686a
Connor <connorbfranklin@gmail.com>
2015-01-31 15:59:31 -0500

    test snap.load

--- js/about.js
+++ js/about.js
@@ -4,6 +4,7 @@ function loadAboutAnimations(){
 	$.getJSON('data/animations-about.json',function(about){
 		//Load SVGs into containers
 		var about1Container = Snap('#about .one')
+		console.log(about.one.file)
 		var about1 = Snap.load(about.one.file, onSVGLoaded(about1Container))
 	});
 }
