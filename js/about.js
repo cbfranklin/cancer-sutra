4f0520adb42c5d94e73734f57a2d8e0045516fed
Connor <connorbfranklin@gmail.com>
2015-02-01 10:30:27 -0500

    refactor

--- js/about.js
+++ js/about.js
@@ -5,34 +5,12 @@ function loadAboutAnimations(){
 		//Load SVGs into containers
 		for(var key in about){
 			var anim = about[key];
-			//console.log(key,anim);
-			//console.log(container)
-			var container = Snap('#about .'+key);
-			/*var svg = Snap.load(anim.file,function(data){
-				//onSVGLoaded(data,container)
-				console.log(container)
-			});*/
 			Snap.load(anim.file,function(key){
 					return function(data){
 						var container = Snap('#about .'+key);
-						console.log(container)
-						onSVGLoaded(data,container)
+						container.append(data);
 					};
 				}(key)
 			);
-
 		}
-
-		/*var about1Container = Snap('#about .one')
-		console.log(about.one.file)
-		var about1 = Snap.load(about.one.file, function(data){
-			onSVGLoaded(data,container)
-		});*/
-	});
-}
-function onSVGLoaded(data,container){
-	//console.log('onSVGLoaded')
-	//console.log(data,container)
-	console.log(container)
-	container.append(data);
 }
